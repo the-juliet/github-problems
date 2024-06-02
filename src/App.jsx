a00e2ac7551454365dcc500748999d9419916983
Juliet486 <juliet486@hanmail.net>
2024-06-02 18:36:28 +0900

    Changed title 2

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div id="logo-img">
           <img src={logo} />
         </div>
-        <h1>PR Test - two</h1>
+        <h1>PR Test - Dev branch </h1>
       </header>
       <MainContent />
     </>
