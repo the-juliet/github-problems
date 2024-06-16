fd9389ed6acd22a45141752162ef3f23fd09d3b2
Juliet486 <juliet486@hanmail.net>
2024-06-16 19:24:20 +0900

    버그  수정

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
           <img src={logo} />
         </div>
         <h1>PR Test - Sunday Comming soon! </h1>
-        <p>Ok! </p>
+        <p>버그 수정 </p>
       </header>
       <MainContent />
     </>
